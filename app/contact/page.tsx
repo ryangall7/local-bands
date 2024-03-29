ea49adebbf42862c08ecd1e094bca08e7a716012
ryangall7 <ryangall7@gmail.com>
2024-03-28 22:29:44 -0600

    type

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -26,7 +26,7 @@ export default function SignIn() {
                 <div className="flex flex-wrap -mx-3 mb-4">
                   <div className="w-full px-3">
                     <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">Name</label>
-                    <input id="email" type="email" className="form-input w-full text-gray-800" name="name" placeholder="Your Name" />
+                    <input id="email" type="text" className="form-input w-full text-gray-800" name="name" placeholder="Your Name" />
                   </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-4">
