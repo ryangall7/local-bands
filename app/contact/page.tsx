a9f275d2b5f6ff5c0907087cfe8ced22415bd9c6
ryangall7 <ryangall7@gmail.com>
2024-03-28 22:23:06 -0600

    ts ignore

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Sign In - Simple',
-  description: 'Page description',
+  title: 'Local Bands - Contact Us',
+  description: 'Let us know your thoughts, about the bands, our website, or the weather.',
 }
 import Image from 'next/image'
 import BandsTexture from '@/public/images/bands-texture.jpg'
@@ -19,43 +19,45 @@ export default function SignIn() {
             <h1 className="h1">Let us know your thoughts.</h1>
           </div>
 
-          {/* Form */}
           <div className="max-w-sm mx-auto bg-white">
-            <form name="contact" netlify>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">Name</label>
-                  <input id="email" type="email" className="form-input w-full text-gray-800" name="name" placeholder="Your Name" />
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email</label>
-                  <input id="email" type="email" className="form-input w-full text-gray-800" name="email" placeholder="Enter your email address" />
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="message">Whats up?</label>
-                  <textarea className="form-textarea w-full text-gray-800" rows={4} name="message" placeholder="Enter your message"></textarea>
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <div className="flex justify-between">
-                    <label className="flex items-center">
-                      <input type="checkbox" className="form-checkbox" name="Subscribe" />
-                      <span className="text-gray-600 ml-2">Let me know about future shenanigans</span>
-                    </label>
+             {
+              //@ts-ignore
+              <form name="contact" netlify>
+                <div className="flex flex-wrap -mx-3 mb-4">
+                  <div className="w-full px-3">
+                    <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">Name</label>
+                    <input id="email" type="email" className="form-input w-full text-gray-800" name="name" placeholder="Your Name" />
                   </div>
                 </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mt-6">
-                <div className="w-full px-3">
-                  <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full" type="submit">Submit</button>
+                <div className="flex flex-wrap -mx-3 mb-4">
+                  <div className="w-full px-3">
+                    <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email</label>
+                    <input id="email" type="email" className="form-input w-full text-gray-800" name="email" placeholder="Enter your email address" />
+                  </div>
                 </div>
-              </div>
-            </form>
+                <div className="flex flex-wrap -mx-3 mb-4">
+                  <div className="w-full px-3">
+                    <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="message">Whats up?</label>
+                    <textarea className="form-textarea w-full text-gray-800" rows={4} name="message" placeholder="Enter your message"></textarea>
+                  </div>
+                </div>
+                <div className="flex flex-wrap -mx-3 mb-4">
+                  <div className="w-full px-3">
+                    <div className="flex justify-between">
+                      <label className="flex items-center">
+                        <input type="checkbox" className="form-checkbox" name="Subscribe" />
+                        <span className="text-gray-600 ml-2">Let me know about future shenanigans</span>
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-wrap -mx-3 mt-6">
+                  <div className="w-full px-3">
+                    <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full" type="submit">Submit</button>
+                  </div>
+                </div>
+              </form>
+            }
             <div className="text-gray-600 text-center mt-6">
               Check us out on <Link href="https://www.facebook.com/LocalBandID" target="blank" className="text-blue-600 hover:underline transition duration-150 ease-in-out">Facebook</Link>
             </div>
