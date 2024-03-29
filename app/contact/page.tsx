80e59675e43beb98e9d7e74883b8cb6e8e7cbb1f
ryangall7 <ryangall7@gmail.com>
2024-03-28 22:11:06 -0600

    submit form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -52,7 +52,7 @@ export default function SignIn() {
               </div>
               <div className="flex flex-wrap -mx-3 mt-6">
                 <div className="w-full px-3">
-                  <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">Submit</button>
+                  <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full" type="submit">Submit</button>
                 </div>
               </div>
             </form>
